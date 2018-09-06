f37c7f833d2afab547cb8ac57751ba73a5bba096
onahump <oswaldo@makingdevs.com>
2018-09-06 14:51:33 -0500

    Adding show's method whitin articles_controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,7 +5,19 @@ class ArticlesController < ApplicationController
 	end
 
 	def create
-		render plain: params[:article].inspect
+		@article = Article.new(article_params)
+
+		@article.save
+		redirect_to @article
 	end
+
+	def show
+		@articlem = Article.find(params[:id])
+	end
+
+	private
+		def article_params
+			params.require(:article).permit(:title, :text)
+		end
 
 end
