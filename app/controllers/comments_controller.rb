75e90bd7504605e479e38295463e17491e8adb8f
onahump <oswaldo@makingdevs.com>
2018-09-07 15:04:25 -0500

    Fixing code errors

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,9 @@
 class CommentsController < ApplicationController
 
 	def create
-		@article = Article.find(params[:id])
+		@article = Article.find(params[:article_id])
 		@comment = @article.comments.create(comment_params)
-		redirect_to article_path
+		redirect_to article_path(@article)
 	end
 
 	private
