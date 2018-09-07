1d67493a820c7584a200ef28f8e365dcaa126ef3
onahump <oswaldo@makingdevs.com>
2018-09-07 17:23:58 -0500

    Adding autentifications in both controllers

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+	 http_basic_authenticate_with name: "onahump", password: "secret", except: [:index, :show]
+
 	def index
 		@articles = Article.all
 	end
