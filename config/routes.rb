a5538106c0dd3309204197217531bf539df80d46
onahump <oswaldo@makingdevs.com>
2018-09-07 14:21:18 -0500

    Adding a comment into code

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   
-  resources :articles
+  resources :articles do
+  	resources :comments
+=begin
+		This creates comments as a nested resource within articles. This is another part of capturing 
+		the hierarchical relationship that exists between articles and comments.
+=end
+  end
 
   root 'welcome#index'
 end
